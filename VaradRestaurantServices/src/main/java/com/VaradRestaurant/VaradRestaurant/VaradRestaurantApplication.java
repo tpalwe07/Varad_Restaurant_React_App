edc283051088ba9d03937afe96570baefed9525a
Yash Deshmukh <yash.deshmukh@rockwellautomation.com>
2025-04-22 22:58:23 +0530

    initial Project Setup

---
+++ VaradRestaurantServices/src/main/java/com/VaradRestaurant/VaradRestaurant/VaradRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.VaradRestaurant.VaradRestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaradRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaradRestaurantApplication.class, args);
+	}
+
+}
---
+++ VaradRestaurantServices/src/test/java/com/VaradRestaurant/VaradRestaurant/VaradRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VaradRestaurant.VaradRestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaradRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
